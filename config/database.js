dae3184e5735c53bfbaa077e8eb15676a6d356b8
carlosdos818@yahoo.com <carlosdos818@yahoo.com>
2025-05-15 20:42:10 -0700

    update: connection to mongodb

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-const MONGO_URI = process.env.MONGO_URI;
+const MONGODB_URI = process.env.MONGODB_URI;
 
-mongoose.connect(MONGO_URI, {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
