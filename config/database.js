ba05594ddda6b35164f6a4bcdeb262631324aa7c
carlosdos818@yahoo.com <carlosdos818@yahoo.com>
2025-05-15 18:48:54 -0700

    Fix: added MongoDB URI support

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(process.env.DATABASE_URL, {
+mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
